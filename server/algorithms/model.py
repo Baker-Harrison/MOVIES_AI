23c6a035ec4cedc902f1c866825fe490deac1231
BachiBachBach <thehessboi@gmail.com>
2025-05-17 14:14:22 -0500

    project setup

---
+++ server/algorithms/model.py
@@ -0,0 +1,8 @@
+# Plot: ...
+# Parameters(bool): { 
+#                     Emotional Engagement: How emotionally invested were you?
+#                     Humor: How funny was it?,
+#                     Rewatchability: Would you want to watch this movie again?, 
+#                   } 
+
+
