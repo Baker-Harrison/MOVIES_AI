331bb159baea82425131274e0a49e36a2bd74887
BachiBachBach <thehessboi@gmail.com>
2025-05-21 12:06:57 -0500

    setup dev middleware

--- server/app.py
+++ server/app.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from server.api.routes.movie import movie_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
     app.include_router(movie_router, prefix="/api/movie")
-    
 
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # In production, replace with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
 
     return app
 
